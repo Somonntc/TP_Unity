c7bc80c571e8786abf167e3315fa32ea2aa533ef
Somon <alexandre.somon@inen.yncrea.fr>
2020-01-22 23:00:49 +0100

    .

---
+++ TP01/Assets/Script/PrefabAnim.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabAnim : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    void Awake(){
+        
+    }
+
+    // Start is call ed before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y > 0){
+            transform.position = new Vector3(
+                transform.position.x,
+                transform.position.y - speed,
+                transform.position.z);
+        }
+        if(transform.position.y <= 0){
+            transform.position = new Vector3(
+                transform.position.x + speed,
+                transform.position.y,
+                transform.position.z);
+        }
+        if(transform.position.x >= 10){
+            Destroy(gameObject);
+        }
+        Debug.Log(transform.position);
+    }
+}
---
+++ TP01/Assets/Script/prefabspawn.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class prefabspawn : MonoBehaviour
+{
+    [SerializeField] private GameObject prefab;
+    [SerializeField] private int quantity;
+    [SerializeField] private float radius;
+    [SerializeField] private float timer;
+
+    private float angle;
+
+    private void Awake(){
+        angle = 0f;
+    }
+
+    // Start is called before the first frame updates
+    void Start()
+    {
+        StartCoroutine(StartLooping());
+    }
+    private IEnumerator StartLooping(){
+        while(true){
+            for(int i = 0; i < quantity; i++){
+                angle = i * Mathf.PI * 2 / quantity;
+                Instantiate(prefab, new Vector3(radius*Mathf.Cos(angle),
+                                                transform.position.y,
+                                                radius*Mathf.Sin(angle)),
+                                                Quaternion.identity,
+                                                transform);
+            }
+            yield return new WaitForSeconds(timer);
+        }
+    }
+}
